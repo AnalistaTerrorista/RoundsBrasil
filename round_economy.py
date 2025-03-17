94fba6aed53d6d60d6ac32fba168b205840db87d
AnalistaTerrorista <terroristaanalista@gmail.com>
2025-03-17 12:05:44 +0000

    Update round_economy.py

--- round_economy.py
+++ round_economy.py
@@ -1,11 +1,15 @@
 
 def addEconomyInRound(rounds, ticks, wanted_ticks):
+    eco_ct = 5100
+    eco_t = 5100
+    semi_ct = 20000
+    semi_t = 20000
     df = ticks[ticks['tick'].isin(wanted_ticks)]
     for index, round in rounds.iterrows():
         subdf = df[df["total_rounds_played"] == round["round_num"] - 1]
         equip_value_ct = subdf[subdf['side'] == 'ct']['current_equip_value'].sum()
         equip_value_t = subdf[subdf['side'] == 't']['current_equip_value'].sum()
-        rounds.loc[index, "CT_equip_value"] = equip_value_ct
-        rounds.loc[index, "T_equip_value"] = equip_value_t
+        rounds.loc[index, "CT_equip_value"] = 'eco' if equip_value_ct < eco_ct else 'semi' if equip_value_ct < semi_ct else 'armado'
+        rounds.loc[index, "T_equip_value"] = 'eco' if equip_value_t < eco_t else 'semi' if equip_value_t < semi_t else 'armado'
     
     return rounds
